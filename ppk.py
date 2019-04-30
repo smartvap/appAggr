b810d68300e77ec75c15ef0efdf7a6ad91a1d8eb
hugh <15069071152@139.com>
2019-04-30 22:09:11 +0800

    .

--- ppk.py
+++ ppk.py
@@ -2,6 +2,12 @@ import selenium
 from selenium import webdriver
 import time
 import cx_Oracle
+import re
+import base64
+from PIL import Image
+import np
+import cv2
+from io import BytesIO
 
 # 字典表
 # lnkPrefix 链接前缀
@@ -12,7 +18,7 @@ dict = {
     'idleTime': '10',
     'db_user': 'c##k8s',
     'db_pass': 'k8s',
-    'db_tns': 'c5csyybcv1'
+    'db_tns': 'cdb1'
 }
 
 # 属性、xpath映射关系
@@ -29,7 +35,7 @@ mapPropXPath = {
 }
 
 # 问题对象：quesObj，属性值是不确定的
-# 属性名称：链接序号(snLink)、题库名称(quesBank)、题目类型(quesType)、问题(quesCont)、选项(opts)
+# 属性名称：链接序号(snLink)、题库名称(quesBank)、题目类型(quesType)、问题(quesCont)、选项(opts)...
 quesObj = {
 }
 
@@ -94,37 +100,79 @@ def closeOtherPages(drv):
 
 # 数据库初始化
 def db_init():
-    try:
-        conn = cx_Oracle.connect(dict['db_user'], dict['db_pass'], dict['db_tns'])
-        cursor = conn.cursor()
-        cursor.execute('CREATE TABLE t_ques' +
-                            '(' +
-                            'sn_lnk     NUMBER(16) NOT NULL,' +
-                            'ques_bank  VARCHAR2(512) NOT NULL,' +
-                            'ques_type  VARCHAR2(512) NOT NULL,' +
-                            'ques_cont  VARCHAR2(4000) NOT NULL,' +
-                            'opts       VARCHAR2(4000),' +
-                            'ans_img_c  CLOB,' +
-                            'interpret  CLOB' +
-                            ')' +
-                            'TABLESPACE D_LOG_05' +
-                            '  PCTFREE 10' +
-                            '  INITRANS 1' +
-                            '  MAXTRANS 255' +
-                            '  STORAGE' +
-                            '  (' +
-                            '    INITIAL 2' +
-                            '    NEXT 2' +
-                            '    MINEXTENTS 1' +
-                            '    MAXEXTENTS UNLIMITED' +
-                            '    PCTINCREASE 0' +
-                            '  )');
-        cursor.close()
-        conn.close()
-        del cursor
-        del conn
-    except cx_Oracle.DatabaseError:
-        print('')
+	try:
+		conn = cx_Oracle.connect(dict['db_user'], dict['db_pass'], dict['db_tns'])
+		cursor = conn.cursor()
+		cursor.execute('CREATE TABLE t_ppk_ques ' +
+		'(' +
+		'  sn_lnk     NUMBER(16) NOT NULL,' +
+		'  ques_bank  VARCHAR2(512) NOT NULL,' +
+		'  ques_type  VARCHAR2(512) NOT NULL,' +
+		'  ques_cont  VARCHAR2(4000) NOT NULL,' +
+		'  opts       VARCHAR2(4000),' +
+		'  ans_img_c  CLOB,' +
+		'  ans_bin_c  CLOB,' +
+		'  interpret  CLOB' +
+		')' +
+		'PARTITION BY RANGE (sn_lnk)' +
+		'(' +
+		'  PARTITION p_1 VALUES LESS THAN (1000000)' +
+		'    TABLESPACE users' +
+		'    PCTFREE 10' +
+		'    INITRANS 40' +
+		'    MAXTRANS 255' +
+		'    STORAGE' +
+		'    (' +
+		'      INITIAL 1M' +
+		'      NEXT 2M' +
+		'      MINEXTENTS 1' +
+		'      MAXEXTENTS UNLIMITED' +
+		'      PCTINCREASE 0' +
+		'    ),' +
+		'  PARTITION p_2 VALUES LESS THAN (2000000)' +
+		'    TABLESPACE users' +
+		'    PCTFREE 10' +
+		'    INITRANS 40' +
+		'    MAXTRANS 255' +
+		'    STORAGE' +
+		'    (' +
+		'      INITIAL 1M' +
+		'      NEXT 2M' +
+		'      MINEXTENTS 1' +
+		'      MAXEXTENTS UNLIMITED' +
+		'      PCTINCREASE 0' +
+		'    ),' +
+		'  PARTITION p_max VALUES LESS THAN (MAXVALUE)' +
+		'    TABLESPACE users' +
+		'    PCTFREE 10' +
+		'    INITRANS 40' +
+		'    MAXTRANS 255' +
+		'    STORAGE' +
+		'    (' +
+		'      INITIAL 1M' +
+		'      NEXT 2M' +
+		'      MINEXTENTS 1' +
+		'      MAXEXTENTS UNLIMITED' +
+		'      PCTINCREASE 0' +
+		'    )' +
+		')')
+		cursor.execute('CREATE UNIQUE INDEX pk_t_ques ON t_ppk_ques (sn_lnk) TABLESPACE users')
+		cursor.execute('CREATE BITMAP INDEX idx_t_ques_bank ON t_ppk_ques(ques_bank) TABLESPACE users LOCAL')
+		cursor.execute('CREATE BITMAP INDEX idx_t_ques_type ON t_ppk_ques(ques_type) TABLESPACE users LOCAL')
+		cursor.execute("COMMENT ON COLUMN t_ppk_ques.sn_lnk IS '问题序号'")
+		cursor.execute("COMMENT ON COLUMN t_ppk_ques.ques_bank IS '题库名称'")
+		cursor.execute("COMMENT ON COLUMN t_ppk_ques.ques_type IS '问题类型'")
+		cursor.execute("COMMENT ON COLUMN t_ppk_ques.ques_cont IS '题干'")
+		cursor.execute("COMMENT ON COLUMN t_ppk_ques.opts      IS '选项'")
+		cursor.execute("COMMENT ON COLUMN t_ppk_ques.ans_img_c IS '答案图片BASE64编码（原始）'")
+		cursor.execute("COMMENT ON COLUMN t_ppk_ques.ans_bin_c IS '答案图片BASE64编码（灰度处理）'")
+		cursor.execute("COMMENT ON COLUMN t_ppk_ques.interpret IS '诠释'")
+		cursor.close()
+		conn.close()
+		del cursor
+		del conn
+	except cx_Oracle.DatabaseError:
+		print('')
 
 # 数据保存
 # sn：问题序号
@@ -133,11 +181,11 @@ def save(sn, ques):
     try:
         conn = cx_Oracle.connect(dict['db_user'], dict['db_pass'], dict['db_tns'])
         cursor = conn.cursor()
-        sql = 'INSERT INTO t_ques(sn_lnk, ques_bank, ques_type, ques_cont, ' +\
-            'opts, ans_img_c, interpret) VALUES (:1, :2, :3, :4, :5, :6, :7)';
+        sql = 'INSERT INTO t_ppk_ques(sn_lnk, ques_bank, ques_type, ques_cont, ' +\
+            'opts, ans_img_c, ans_bin_c, interpret) VALUES (:1, :2, :3, :4, :5, :6, :7, :8)';
         cursor.prepare(sql)
         cursor.execute(sql, (sn, ques['quesBank'], ques['quesType'], ques['quesCont'], \
-                             ques['opts'], ques['ansImgBase64'], ques['interpretation']))
+                             ques['opts'], ques['ansImgBase64'], ques['ansGrayBase64'], ques['interpretation']))
         conn.commit()
         cursor.close()
         conn.close()
@@ -145,45 +193,88 @@ def save(sn, ques):
         del cursor
         del conn
     except cx_Oracle.DatabaseError:
-        print('')
+        print('数据库访问异常')
+
+# 显示答案灰度图象版本
+def dispAnsGray(sn):
+	try:
+		conn = cx_Oracle.connect(dict['db_user'], dict['db_pass'], dict['db_tns'])
+		cursor = conn.cursor()
+		sql = 'SELECT ans_bin_c FROM t_ppk_ques WHERE sn_lnk = :1'
+		cursor.prepare(sql)
+		cursor.execute(sql, [ sn ])
+		for rec in cursor:
+			base64_str = rec[0].read()
+		cursor.close()
+		conn.close()
+		del sql
+		del cursor
+		del conn
+		base64_data = re.sub('^data:image/.+;base64,', '', base64_str)
+		byte_data = base64.b64decode(base64_data)
+		image_data = BytesIO(byte_data)
+		img = Image.open(image_data)
+		img.show()
+	except cx_Oracle.DatabaseError:
+		print('')
+
+# pip install pillow
+# pip install np
+# pip install numpy
+# pip install opencv-python
+
+
+# 对原始图像Base64编码解码、灰度处理、再转换为Base64编码返回
+# Base64编码入库CLOB字段时，不能带b头部，应去掉。
+def getGrayBase64(base64_str):
+	base64_data = re.sub('^data:image/.+;base64,', '', base64_str)
+	byte_data = base64.b64decode(base64_data)
+	imgArr = np.frombuffer(byte_data, np.uint8)
+	img = cv2.imdecode(imgArr, cv2.COLOR_BGR2RGB)
+	ret, thresh1 = cv2.threshold(img[:,:,0], 90, 255, cv2.THRESH_BINARY)
+	img = Image.fromarray(thresh1)
+	output_buffer = BytesIO()
+	img.save(output_buffer, format = 'JPEG')
+	byte_data = output_buffer.getvalue()
+	base64_str = base64.b64encode(byte_data)
+	return base64_str.decode('ascii')
 
 # 主循环
-for i in range(72, 100):
-    closeOtherPages(driver)
-    print('只保留一个页签...')
-    driver.get(dict['lnkPrefix'] + str(i) + dict['lnkSuffix'])
-    print('打开问题' + str(i) + '...')
-    if not 'quesObj' in locals():
-        quesObj = {}
-    parseQues(driver, mapPropXPath['quesBank'], 'innerText', quesObj, 'quesBank', 0)
-    print('获取题库名称...')
-    parseQues(driver, mapPropXPath['quesType'], 'innerText', quesObj, 'quesType', 0)
-    print('获取问题类型...')
-    parseQues(driver, mapPropXPath['quesCont'], 'innerText', quesObj, 'quesCont', 0)
-    print('获取问题内容...')
-    if quesObj['quesType'].find('选择题') >= 0:
-        parseQues(driver, mapPropXPath['opts'], 'innerText', quesObj, 'opts', 0)
-        print('获取问题选项...')
-        while driver.window_handles.__len__() < 2:
-            driver.execute_script("ViewAnswers('//api.ppkao.com/mnkc/tiku/?id=" + str(i) + "','432')")
-            time.sleep(5)
-        doSwitchPage(driver)
-        print('页面切换到答案页面...')
-        closeOtherPages(driver)
-        print('关闭其他页面...')
-        parseQues(driver, mapPropXPath['ansImgBase64'], 'src', quesObj, 'ansImgBase64', 0)
-        print('获取答案图片BASE64编码...')
-        parseQues(driver, mapPropXPath['interpretation'], 'innerText', quesObj, 'interpretation', 1)
-        print('获取诠释...')
-    elif quesObj['quesType'].find('问答题') >= 0 or quesObj['quesType'].find('填空题') >= 0:
-        parseQues(driver, mapPropXPath['ansTxt'], 'innerText', quesObj, 'opts', 0)
-        quesObj['ansImgBase64'] = ''
-        quesObj['interpretation'] = ''
-    save(i, quesObj)
-    print('问题' + str(i) + '已保存')
-    del quesObj
-    time.sleep(5)
-
-
 driver = webdriver.Chrome()
-driver.get(dict['lnkPrefix'] + str(1) + dict['lnkSuffix'])
+for i in range(1, 2):
+	closeOtherPages(driver)
+	print('只保留一个页签...')
+	driver.get(dict['lnkPrefix'] + str(i) + dict['lnkSuffix'])
+	print('打开问题' + str(i) + '...')
+	if not 'quesObj' in locals():
+		quesObj = {}
+	parseQues(driver, mapPropXPath['quesBank'], 'innerText', quesObj, 'quesBank', 0)
+	print('获取题库名称...')
+	parseQues(driver, mapPropXPath['quesType'], 'innerText', quesObj, 'quesType', 0)
+	print('获取问题类型...')
+	parseQues(driver, mapPropXPath['quesCont'], 'innerText', quesObj, 'quesCont', 0)
+	print('获取问题内容...')
+	if quesObj['quesType'].find('选择题') >= 0:
+		parseQues(driver, mapPropXPath['opts'], 'innerText', quesObj, 'opts', 0)
+		print('获取问题选项...')
+		while driver.window_handles.__len__() < 2:
+			driver.execute_script("ViewAnswers('//api.ppkao.com/mnkc/tiku/?id=" + str(i) + "','432')")
+			time.sleep(5)
+		doSwitchPage(driver)
+		print('页面切换到答案页面...')
+		closeOtherPages(driver)
+		print('关闭其他页面...')
+		parseQues(driver, mapPropXPath['ansImgBase64'], 'src', quesObj, 'ansImgBase64', 0)
+		print('获取答案图片BASE64编码...')
+		quesObj['ansGrayBase64'] = getGrayBase64(quesObj['ansImgBase64'])
+		print('灰度处理...')
+		parseQues(driver, mapPropXPath['interpretation'], 'innerText', quesObj, 'interpretation', 1)
+		print('获取诠释...')
+	elif quesObj['quesType'].find('问答题') >= 0 or quesObj['quesType'].find('填空题') >= 0:
+		parseQues(driver, mapPropXPath['ansTxt'], 'innerText', quesObj, 'opts', 0)
+		quesObj['ansImgBase64'] = ''
+		quesObj['interpretation'] = ''
+	save(i, quesObj)
+	print('问题' + str(i) + '已保存')
+	#del quesObj
+	time.sleep(5)
